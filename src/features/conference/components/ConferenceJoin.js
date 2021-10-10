263e7da9f797b8ba8a02b41c1b56ac0d9b694451
dmdraghici@gmail.com <dmdraghici@gmail.com>
2021-10-11 01:56:01 +0300

    UpdatesonJoinedConferenceScreen

--- src/features/conference/components/ConferenceJoin.js
+++ src/features/conference/components/ConferenceJoin.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-bind */
 
 import React, { useEffect, useReducer } from "react"
 import { useTranslation } from "react-i18next"
@@ -15,77 +16,79 @@ import { useQueryWithErrorHandling } from "hooks/errorHandling";
 import { CONFERENCE_QUERY } from "features/myConference/gql/queries/conferenceQueries";
 import CancelButton from "@bit/totalsoft_oss.react-mui.cancel-button";
 import LoadingFakeText from "@bit/totalsoft_oss.react-mui.fake-text/dist/LoadingFakeText";
+import VideoCameraFrontSharpIcon from '@mui/icons-material/VideoCameraFrontSharp';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import MicSharpIcon from '@mui/icons-material/MicSharp';
+import PanToolSharpIcon from '@mui/icons-material/PanToolSharp';
+import video1 from "../../../assets/img/video1.mp4";
+import { Widget } from 'react-chat-widget';
+import PersonIcon from '@mui/icons-material/Person';
+import 'react-chat-widget/lib/styles.css';
 
-// const ConferenceJoin = ({ code, suggestedConferences, onAttend }) => {
-//   const { t } = useTranslation()
-//   const [, setHeader] = useHeader()
-//   const [conference, dispatch] = useReducer(reducer, initialConference)
-//   const { name } = conference
-//   const match = useRouteMatch();
-//   const conferenceId = match.params.id;
-//   const isNew = conferenceId === 'new';
-
-//   const { data, loading } = useQueryWithErrorHandling(CONFERENCE_QUERY, {
-//     variables: { id: conferenceId, isNew },
-//     onCompleted: result => result?.conference && dispatch({ type: 'resetConference', payload: result.conference })
-//   })
-
-//   useEffect(() => () => setHeader(null), [])  // eslint-disable-line react-hooks/exhaustive-deps
-//   useEffect(() => { setHeader(<MyConferenceHeader title={name} />) }, [setHeader, t, name])
-//   return (
-//     <>
-//       <Grid container>
-//         <Grid item lg={12}>
-//           <img src={qrCode} style={{ maxHeight: '400px' }} alt='QR' />
-//         </Grid>
-//         <Grid item lg={12}>
-//           <Typography>{t("Conferences.QRCodeMessage", { code })}</Typography>
-//         </Grid>
-//       </Grid>
-//     </>
-//   )
-// }
-
-
-// ConferenceJoin.propTypes = {
-//   code: PropTypes.string,
-//   suggestedConferences: PropTypes.array,
-//   onAttend: PropTypes.func
-// }
-
-// export default ConferenceJoin
 
 const ConferenceJoin = () => {
 
-  const [conference, dispatch] = useReducer(reducer, initialConference)
-  const {name} = conference
-  const match = useRouteMatch()
-  const { t } = useTranslation()
-  const [, setHeader] = useHeader()
-  const history = useHistory()
+    const [conference, dispatch] = useReducer(reducer, initialConference)
+    const { name } = conference
+    const match = useRouteMatch()
+    const { t } = useTranslation()
+    const [, setHeader] = useHeader()
+    const history = useHistory()
 
-  const conferenceId = match.params.id;
-  const isNew = conferenceId === 'new';
+    const conferenceId = match.params.id;
+    const isNew = conferenceId === 'new';
+    const [value, setValue] = React.useState(0);
+    const handleNewUserMessage = (newMessage) => {
+        console.log(`New message incoming! ${newMessage}`);
+        // Now send the message throught the backend API
+    };
 
-  const { data, loading: loadingConference } = useQueryWithErrorHandling(CONFERENCE_QUERY, {
-      variables: { id: conferenceId, isNew },
-      onCompleted: result => result?.conference && dispatch({ type: 'resetConference', payload: result.conference })
-  })
-
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
   
 
-  useEffect(() => () => setHeader(null), [])  // eslint-disable-line react-hooks/exhaustive-deps
-  useEffect(() => {
-      setHeader(<MyConferenceHeader title={name} actions={<CancelButton title={t('General.Button.Close')} onClick={history.goBack} />} />)
-  }, [ history.goBack, t, setHeader, name])
 
-  if (loadingConference)
-  return <LoadingFakeText lines={10} />
+    const { data, loading: loadingConference } = useQueryWithErrorHandling(CONFERENCE_QUERY, {
+        variables: { id: conferenceId, isNew },
+        onCompleted: result => result?.conference && dispatch({ type: 'resetConference', payload: result.conference })
+    })
 
 
-  return(
-      < >OK</> )
-     
-  
+
+    useEffect(() => () => setHeader(null), [])  // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        setHeader(<MyConferenceHeader title={name} actions={<CancelButton title={t('General.Button.Close')} onClick={history.goBack} />} />)
+    }, [history.goBack, t, setHeader, name])
+
+    if (loadingConference)
+        return <LoadingFakeText lines={10} />
+
+
+    return (
+        < >
+            <Grid container alignItems="center" justifyContent="center">
+                <Grid item >
+                    <video src={video1} width="1200" height="600" controls="controls" autoPlay="true" />
+                </Grid>
+                <Grid item lg={3} sm={6} xs={12}>
+                    <Tabs value={value} onChange={handleChange} aria-label="icon tabs example">
+                        <Tab icon={<VideoCameraFrontSharpIcon />} aria-label="camera" />
+                        <Tab icon={<MicSharpIcon />} aria-label="microphone" />
+                        <Tab icon={<PanToolSharpIcon />} aria-label="raisehand" />
+                    </Tabs>
+                </Grid>
+                <Grid item >
+                    <Widget handleNewUserMessage={handleNewUserMessage} profileAvatar={PersonIcon}
+                        title="Titlul conferintei "
+                        subtitle="Live chat"
+                    />
+
+                </Grid>
+            </Grid>
+        </>)
+
+
 }
 export default ConferenceJoin
